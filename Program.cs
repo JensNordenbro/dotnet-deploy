2bbff17959de42e9d7f0630e2de520709d1ab307
JensNordenbro <jens.nordenbro@gmail.com>
2017-04-25 20:46:35 +0200

    scafolded project

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace dotnet_deploy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = string.Empty;
+            foreach (string arg in args)
+            {
+                s += arg + ",";
+            }
+            Console.WriteLine("Deploy: " + s);
+        }
+    }
+}
